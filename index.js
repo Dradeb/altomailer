d4c7235456377b7528df098218c51630ab5da0f7
Youness Dradeb <youness.dradeb@gmail.com>
2025-02-05 16:29:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-index.js                                                                                                 
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
